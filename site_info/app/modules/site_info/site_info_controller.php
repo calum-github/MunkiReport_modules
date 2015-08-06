95d8b05862102004124051a4c1f8ab5a9cd91f5c
Arjen van Bochoven <arjen@bochoven.net>
2015-08-06 15:30:31 +0200

    Add two accessors to site_info_controller
    JSON API accessors to get data

--- site_info/app/modules/site_info/site_info_controller.php
+++ site_info/app/modules/site_info/site_info_controller.php
@@ -6,7 +6,7 @@
  * @package munkireport
  * @author
  **/
-class site_info_controller extends Module_controller
+class Site_info_controller extends Module_controller
 {
         
         /*** Protect methods with auth! ****/
@@ -14,6 +14,7 @@ class site_info_controller extends Module_controller
         {
                 // Store module path
                 $this->module_path = dirname(__FILE__);
+                $this->view_path = $this->module_path . '/views/';
         }
 
         /**
@@ -25,8 +26,45 @@ class site_info_controller extends Module_controller
         {
                 echo "You've loaded the site_info module!";
         }
+        
+        /**
+         * Get data for serial
+         *
+         * Retrieves data for serial number and returns that as JSON
+         *
+         * @param string serial_number
+         **/
+        public function get_data($serial_number="")
+        {
+            $obj = new View();
+
+            if( ! $this->authorized())
+            {
+                $obj->view('json', array('msg' => 'Not authorized'));
+            }
+
+            $site_info = new Site_info_model($serial_number);
+            $obj->view('json', array('msg' => $site_info->rs));
+        }
+        
+        /**
+         * Show listing
+         *
+         **/
+        function listing()
+        {
+            if( ! $this->authorized())
+            {
+                redirect('auth/login');
+            }
+            
+            $data['page'] = '';
+            $data['scripts'] = array("clients/client_list.js");
+            $obj = new View();
+            $obj->view('site_info_listing', $data, $this->view_path);
+        }
+
 
         
 } // END class default_module
-
 
